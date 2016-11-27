2c97e10d2cb910e05dc79d1a79fdf1f24f26f0ae
James Sharp <james@ortootech.com>
2016-11-27 19:07:28 +0000

    allow clients to choose cipher suite

--- linkerd-proxy.go
+++ linkerd-proxy.go
@@ -69,9 +69,8 @@ func main() {
 		Addr:    ":" + port,
 		Handler: redirect,
 		TLSConfig: &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
